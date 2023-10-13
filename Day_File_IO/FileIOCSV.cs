69e23a5ef9fdd6fd3f02224239920a15c60dc362
hareesh <hareesh.vanapalli.888@gmail.com>
2023-10-13 20:27:42 +0530

    [hari]

---
+++ Day_File_IO/FileIOCSV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_File_IO
+{
+    internal class FileIOCSV
+    {
+        public void File_IO__CSV()
+        {
+
+            // Example data for CSV file
+            string FilePath = @"C:\Users\haree\source\repos\Day_File_IO\Day_File_IO\CSV.csv";
+
+            string csvData = "Name,Age,Location\nhari,22,india\nnaidu,25,Los Angeles\nvanapalli,16,UK";
+
+            // Write to a CSV file
+            File.WriteAllText(FilePath, csvData);
+
+            // Read from a CSV file
+            string csvContent = File.ReadAllText(FilePath);
+
+            Console.WriteLine("CSV File Content:\n" + csvContent);
+
+
+        }
+    }
+}
---
+++ Day_File_IO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Day_File_IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileIOText fileIOText = new FileIOText();
+            fileIOText.File_IOText();
+
+            FileIOCSV fileIOCSV = new FileIOCSV();
+            fileIOCSV.File_IO__CSV();
+
+            FileIO_JSON fileIO_JSON = new FileIO_JSON();    
+            fileIO_JSON.File_IOJSON();
+        }
+    }
+}
+    
